d90d07d0141236f8c5fbe36d1862132cc27eb724
Bruno Leonel Roca <brunoleonelroca@gmail.com>
2026-05-26 22:51:31 -0300

    Agregar SessionManager usando Singleton

---
+++ SGVCelulares/Servicios/SER_SesionManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servicios
+{
+    public class SER_SesionManager
+    {
+        public SER_Usuario Usuario { get; set; }
+        private static SER_SesionManager sesion;
+        private SER_SesionManager() { }
+        static public SER_SesionManager ObtenerSesion()
+        {
+            if (sesion == null)
+            {
+                sesion = new SER_SesionManager();
+            }
+            return sesion;
+        }
+    }
+}
